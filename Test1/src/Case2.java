d8d658893ca6e20fed86bd2d1e2ea043f13c0c4d
nayanmital <mittalnayan5@gmail.com>
2020-01-10 21:38:04 +0530

    222

---
+++ Test1/src/Case2.java
@@ -0,0 +1,13 @@
+
+public class Case2 {
+
+	public Case2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Case2");
+	}
+
+}
