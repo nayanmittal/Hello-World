73fa2444d5e6785286167012ccfcead89c067d08
nayanmital <mittalnayan5@gmail.com>
2020-01-10 21:35:31 +0530

    first commit

---
+++ Test1/src/Case1.java
@@ -0,0 +1,13 @@
+
+public class Case1 {
+
+	public Case1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public static void main(String args[])
+	{
+		System.out.println("nayan mittal");
+	}
+}
